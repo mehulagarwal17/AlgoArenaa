92c7d82867a494e868097377659db0a38e6469f7
Mehul Agarwal <mehulagarwal11111@gmail.com>
2025-09-20 07:55:34 +0000

    same error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,16 +2,16 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDYTuuQoK-y1q4NBnLnLQ7KTdwm74Kv-aA',
-  authDomain: 'studio-542707319-af169.firebaseapp.com',
-  projectId: 'studio-542707319-af169',
-  storageBucket: 'studio-542707319-af169.appspot.com',
-  messagingSenderId: '1098936362283',
-  appId: '1:1098936362283:web:a56b4024ddf882330c516e',
+  apiKey: "AIzaSyDYTuuQoK-y1q4NBnLnLQ7KTdwm74Kv-aA",
+  authDomain: "studio-542707319-af169.firebaseapp.com",
+  projectId: "studio-542707319-af169",
+  storageBucket: "studio-542707319-af169.appspot.com",
+  messagingSenderId: "1098936362283",
+  appId: "1:1098936362283:web:a56b4024ddf882330c516e",
 };
 
 // Initialize Firebase
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
 export { app, auth };
